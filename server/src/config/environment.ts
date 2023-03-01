2958357aa83aa329a35670e790efa2be070ed34c
zoel-a <zulmyazhary32@gmail.com>
2023-03-01 22:11:31 +0800

    Re-configuration jwt_public & jwt_private key

--- server/src/config/environment.ts
+++ server/src/config/environment.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 
 const CONFIG = {
   db: process.env.DB,
-  jwt_public: process.env.JWT_PUBLIC,
-  jwt_private: process.env.JWT_PRIVATE
+  jwt_public: process.env.JWT_PUBLIC?.replace(/\\n/g, "\n"),
+  jwt_private: process.env.JWT_PRIVATE?.replace(/\\n/g, "\n")
 };
 
 export default CONFIG;
