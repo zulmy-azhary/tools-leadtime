3c57c0b38ec87dd3817820963055256eacbe6106
Muhammad Ridwan <ridhoang93@gmail.com>
2023-03-30 01:14:30 +0700

    add breadcrumb component

---
+++ client/src/components/atoms/BreadCrumb.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { AiFillHome } from "react-icons/ai";
+import { IoChevronForwardOutline } from "react-icons/Io5";
+
+const Breadcrumb: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <div className="flex items-center gap-x-4 font-semibold">
+      <AiFillHome className="text-lg" />
+      <IoChevronForwardOutline className="text-lg" />
+      <span className="capitalize">{children}</span>
+    </div>
+  );
+};
+
+export default Breadcrumb;
