6ad3a11acd4fa4d94070c2e41b6a26db4685413b
Muhammad Ridwan <ridhoang93@gmail.com>
2023-03-30 01:45:33 +0700

    rename ImageWrapper to WrapperImage

---
+++ client/src/components/molecules/WrapperImage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ImageBanner } from "../atoms";
+
+interface Props {
+  src: string;
+}
+
+const ImageWrapper: React.FC<Props> = props => {
+  const { src } = props;
+  return (
+    <div className="order-first col-span-3 lg:order-last ">
+      <ImageBanner src={src} />
+    </div>
+  );
+};
+
+export default ImageWrapper;
