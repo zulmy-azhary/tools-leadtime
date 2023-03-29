e5075dfc5b6cd74990197f83347e0e1eae10cbb3
Muhammad Ridwan <ridhoang93@gmail.com>
2023-03-30 01:59:28 +0700

    modify some layout style for main menu

--- client/src/components/organisms/ProtectedContainer.tsx
+++ client/src/components/organisms/ProtectedContainer.tsx
@@ -3,11 +3,9 @@ import { Sidebar, Navbar } from ".";
 
 const ProtectedContainer: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <main className="flex min-h-screen w-full gap-x-6 px-6 lg:gap-x-12 lg:px-12 xl:gap-x-16 xl:px-16">
-      <aside className="relative md:basis-48 lg:basis-52 xl:basis-56">
-        <Sidebar />
-      </aside>
-      <section className="col-span-full flex h-[2000px] min-h-screen grow flex-col gap-y-6 py-5 md:col-span-8 lg:col-span-9 xl:col-span-10 xl:gap-y-12">
+    <main className="flex min-h-screen w-full">
+      <Sidebar />
+      <section className="ml-[300px] flex min-h-screen grow flex-col gap-y-5 px-8">
         <Navbar />
         {children}
       </section>
