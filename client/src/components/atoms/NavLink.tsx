e956e1feeabefe7bb0e11946ec27ac508abffd93
Muhammad Ridwan <ridhoang93@gmail.com>
2023-03-30 01:21:25 +0700

    modify navlink colors & etc

--- client/src/components/atoms/NavLink.tsx
+++ client/src/components/atoms/NavLink.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React from "react";
 import type { IconType } from "react-icons/lib";
 import { NavLink as BaseNavLink, type NavLinkProps } from "react-router-dom";
@@ -10,27 +11,22 @@ const NavLink: React.ForwardRefRenderFunction<HTMLAnchorElement, Props> = (props
   const { to, icon: Icon, children, className, ...rest } = props;
 
   return (
-    <BaseNavLink
-      ref={ref}
-      to={to}
-      className={({ isActive }) =>
-        [
-          className,
-          isActive
-            ? "bg-cardLight dark:bg-cardDark group rounded-md text-indigo-500 shadow-sm dark:text-teal-400"
-            : "opacity-50"
-        ]
-          .filter(Boolean)
-          .join(" ")
-      }
-      {...rest}
-    >
-      <div className="flex items-center gap-x-4 px-6 py-3 text-indigo-500 dark:text-teal-400">
-        <div className="bg-cardLight dark:bg-cardDark group-visited:text-bgLight dark:group-visited:text-bgDark rounded-md p-2 group-visited:bg-indigo-500 dark:group-visited:bg-teal-400">
-          <Icon className="text-lg" />
+    <BaseNavLink ref={ref} to={to} {...rest}>
+      {({ isActive }) => (
+        <div
+          className={clsx(
+            "flex items-center gap-x-4 rounded p-[10px] text-sm",
+            isActive ? "bg-blue-500/30 text-blue-500" : "text-slate-600"
+          )}
+        >
+          <div
+            className={clsx("rounded p-2", isActive ? "bg-blue-500 text-white drop-shadow-none" : "bg-white shadow-md")}
+          >
+            <Icon className="text-lg" />
+          </div>
+          <span>{children}</span>
         </div>
-        <span>{children}</span>
-      </div>
+      )}
     </BaseNavLink>
   );
 };
