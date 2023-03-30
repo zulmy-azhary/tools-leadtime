8f09f08c1e0d19174b33d912ff1173f7682115a8
Muhammad Ridwan <ridhoang93@gmail.com>
2023-03-31 05:20:28 +0700

    add routing for flow process page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { Loading } from "./components/atoms";
 import { MainLayout } from "./components/templates";
 import { AuthProvider } from "./context";
 import { Toaster } from "./components/molecules";
+import { FlowProcess } from "./components/pages";
 
 const Login = React.lazy(async () => await import("./components/pages").then(comp => ({ default: comp.Login })));
 const Register = React.lazy(async () => await import("./components/pages").then(comp => ({ default: comp.Register })));
@@ -52,6 +53,7 @@ const router = createBrowserRouter(
       >
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/karyawan" element={<Employees />} />
+        <Route path="/flowproses" element={<FlowProcess />} />
         <Route path="*" element={<Custom404 />} />
       </Route>
     </>
