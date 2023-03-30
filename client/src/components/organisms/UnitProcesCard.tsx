d52693316fe7bc6f9b6797cfda33bcf314113c56
Muhammad Ridwan <ridhoang93@gmail.com>
2023-03-31 04:33:37 +0700

    adding a container to wrap the process unit card

---
+++ client/src/components/organisms/UnitProcesCard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ContentWrapper, Headers, UnitCard } from "../molecules";
+import { IoBuild } from "react-icons/io5";
+
+const proses: string[] = ["Ketokan", "Putty", "Removal", "Masking", "Epoxy", "Spraying", "Assembling", "Polishing"];
+
+const UnitProcesCard: React.FC = () => {
+  return (
+    <>
+      <ContentWrapper className="grid grid-flow-dense gap-x-6 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <Headers headerTitle="Dashboard" description="Leadtime & Paint" className="col-span-full" />
+        {proses.map((item, idx) => (
+          <UnitCard key={idx} icon={IoBuild} title={item} unitValue={0} subTitle="Proses Unit" />
+        ))}
+      </ContentWrapper>
+    </>
+  );
+};
+
+export default UnitProcesCard;
