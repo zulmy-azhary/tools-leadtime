9b90d5634ade906ce4570dfb44cfc8df9403608a
Muhammad Ridwan <ridhoang93@gmail.com>
2023-03-31 06:06:28 +0700

    add data unit page

---
+++ client/src/components/pages/DataUnit.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useDocumentTitle } from "../../hooks/useDocumentTitle";
+import { DataUnitContainer } from "../organisms";
+
+const DataUnit: React.FC = () => {
+  useDocumentTitle("Unit");
+  return (
+    <section>
+      <DataUnitContainer />
+    </section>
+  );
+};
+
+export default DataUnit;
