945402649a5667d45fd811459ca01d1bf8dcc0ef
Muhammad Ridwan <ridhoang93@gmail.com>
2023-04-05 03:51:37 +0700

    update modals overlay for responsive scroll

--- client/src/components/molecules/Modal.tsx
+++ client/src/components/molecules/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import clsx from "clsx";
 import { useOnClickOutside } from "../../hooks";
 import { Button, Heading } from "../atoms";
@@ -13,18 +13,33 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
 const Modal: React.FC<Props> = props => {
   const { children, isOpen, onToggle, modalTitle, className } = props;
   const modalRef = useRef(null);
+
   useOnClickOutside(modalRef, () => isOpen && onToggle());
+
+  useEffect(() => {
+    const body = document.body.style;
+    if (isOpen) body.overflow = "hidden";
+    return () => {
+      body.overflow = "auto";
+    };
+  }, [isOpen]);
+
   return isOpen ? (
-    <div className={clsx("fixed inset-0 z-50 flex items-center justify-center bg-gray-700/20 p-6 backdrop-blur-sm")}>
-      <div
-        ref={modalRef}
-        className={clsx("h-fit max-w-3xl grow rounded-lg border-[1.6px] border-gray-300 bg-white p-6", className)}
-      >
-        <div className={clsx("flex")}>
-          <Heading className={clsx("grow text-xl font-semibold")}>{modalTitle}</Heading>
-          <Button icon={IoClose} onClick={() => onToggle()} />
+    <div className={clsx("fixed inset-0 z-50 bg-gray-700/20 backdrop-blur-sm")}>
+      <div className={clsx("flex h-full items-center overflow-y-auto py-6")}>
+        <div
+          ref={modalRef}
+          className={clsx(
+            "m-auto flex max-w-3xl flex-col gap-7 rounded-lg border-[1.6px] border-gray-300 bg-white p-6",
+            className
+          )}
+        >
+          <div className={clsx("flex")}>
+            <Heading className={clsx("grow text-xl font-semibold")}>{modalTitle}</Heading>
+            <Button icon={IoClose} onClick={() => onToggle()} />
+          </div>
+          {children}
         </div>
-        {children}
       </div>
     </div>
   ) : null;
