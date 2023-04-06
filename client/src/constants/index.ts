b82a554655715e8e7337404feb007970298dcb4d
zoel-a <zulmyazhary32@gmail.com>
2023-04-06 20:25:40 +0800

    Add constants variable

---
+++ client/src/constants/index.ts
@@ -0,0 +1,27 @@
+export const DAMAGE: string[] = ["Ringan", "Sedang", "Berat"];
+
+export const VENDOR: string[] = ["WIS", "SPA"];
+
+export const PROCESS: string[] = [
+  "Tunggu Teknisi",
+  "Tunggu Part",
+  "Ketokan",
+  "Removal",
+  "Putty",
+  "Epoxy",
+  "Masking",
+  "Spraying",
+  "Assembling",
+  "Polishing"
+];
+
+export const ADVISOR: string[] = [
+  "Ahmad Supardi",
+  "Fadli",
+  "Hariyadi",
+  "Imam",
+  "Muhammad Fachri",
+  "Musrin Noor",
+  "Reza",
+  "Syamsuryanan Amir"
+];
