148aa8313b8e8128383c510fb49fefa4dbcd9fa9
zoel-a <zulmyazhary32@gmail.com>
2023-04-11 01:08:57 +0800

    Add axios for protected services

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,6 +1,18 @@
 import axios from "axios";
 import { SERVER_BASE_URL } from "../helpers/constants";
+import Cookies from "js-cookie";
 
 export default axios.create({
   baseURL: SERVER_BASE_URL
 });
+
+export const axiosProtected = () => {
+  const accessToken = Cookies.get("accessToken");
+
+  return axios.create({
+    baseURL: SERVER_BASE_URL,
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  });
+};
