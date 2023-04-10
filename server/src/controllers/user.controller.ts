d409e176a18bbb2cfc4851ec666df0bd2eb10f26
zoel-a <zulmyazhary32@gmail.com>
2023-04-10 22:54:02 +0800

    Modify error logger

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -3,14 +3,14 @@ import { logger } from "../utils/logger";
 
 export const getMe = async (req: Request, res: Response) => {
   try {
-    logger.info("USER -> GET ME = User identified.");
+    logger.info("USER -> GET_ME = User identified.");
     return res.status(200).send({
       status: true,
       statusCode: 200,
       message: "User is authenticated."
     });
   } catch (err) {
-    logger.error(`USER -> GET ME = ${(err as Error).message}`);
+    logger.error(`USER -> GET_ME = ${(err as Error).message}`);
     return res.status(500).send({ status: false, statusCode: 500, message: (err as Error).message });
   }
 };
