177f9237bbbce045a8e00471e4cebc6620891f87
Muhammad Ridwan <ridhoang93@gmail.com>
2023-04-14 04:48:36 +0700

    add conditional size for sidebar toggler, add close button, modify sidebar heading

--- client/src/components/organisms/Sidebar.tsx
+++ client/src/components/organisms/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import clsx from "clsx";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context";
 import { toast } from "react-hot-toast";
@@ -7,9 +8,18 @@ import { useMutation } from "react-query";
 import type { AxiosError, AxiosResponse } from "axios";
 import { logout } from "../../api/auth";
 import type { TResponse } from "../../types";
-import { Button } from "../atoms";
+import { Button, Heading } from "../atoms";
+import { useMediaQuery } from "../../hooks";
+import { IoClose } from "react-icons/io5";
 
-const Sidebar: React.FC = () => {
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const Sidebar: React.FC<Props> = props => {
+  const { isOpen, onClose } = props;
+  const isNotebook = useMediaQuery("(max-width: 1024px)");
   const { user, logout: handleLogout } = useAuth();
   const navigate = useNavigate();
 
@@ -31,9 +41,35 @@ const Sidebar: React.FC = () => {
   };
 
   return (
-    <aside className="fixed inset-y-0 flex w-[300px] flex-col justify-between px-6 pb-6">
+    <aside
+      className={clsx(
+        "bg-bgLight fixed inset-y-0 z-10 flex flex-col justify-between overflow-hidden dark:bg-slate-800",
+        isOpen ? "w-[300px] px-6 pb-6" : "w-0 p-0"
+      )}
+    >
       <div className="flex flex-col gap-y-5">
-        <div className="py-7 text-center text-xl font-semibold uppercase tracking-[0.25em]">Kalla Toyota</div>
+        <div className="flex items-center py-7">
+          <Heading
+            className={clsx(
+              "grow text-xl font-semibold uppercase tracking-[0.25em]",
+              isNotebook ? "text-left" : "text-center"
+            )}
+          >
+            Kalla Toyota
+          </Heading>
+          {isNotebook && (
+            <Button
+              icon={IoClose}
+              onClick={onClose}
+              className={clsx(
+                "border-[1.6px] p-1 text-sm",
+                "bg-gray-50 dark:bg-gray-800",
+                "border-gray-200 dark:border-gray-700",
+                "text-gray-600 dark:text-gray-400"
+              )}
+            />
+          )}
+        </div>
         <SidebarList />
       </div>
       <Button
