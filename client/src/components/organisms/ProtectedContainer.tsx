74d8a5c9161ca58f459e6365264bc800123dffec
Muhammad Ridwan <ridhoang93@gmail.com>
2023-04-14 04:45:17 +0700

    modify layout for responsive sidebar & main layout

--- client/src/components/organisms/ProtectedContainer.tsx
+++ client/src/components/organisms/ProtectedContainer.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Sidebar, Navbar } from ".";
+import { useMediaQuery, useToggle } from "../../hooks";
+import clsx from "clsx";
 
 const ProtectedContainer: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const [isOpen, toggle, setIsOpen] = useToggle();
+  const isNotebook = useMediaQuery("(max-width: 1024px)");
+
+  useEffect(() => {
+    if (isNotebook) setIsOpen(false);
+    return () => setIsOpen(true);
+  }, [isNotebook]);
   return (
     <>
-      <Sidebar />
-      <main className="ml-[300px] flex min-h-screen grow flex-col gap-y-5 px-8 pb-12">
-        <Navbar />
+      <Sidebar isOpen={isOpen} onClose={() => setIsOpen(false)} />
+      <main className={clsx("flex min-h-screen flex-col gap-y-5 px-8 pb-12", isNotebook ? "ml-[0px]" : "ml-[300px]")}>
+        <Navbar sidebarToggle={toggle} />
         <section className="grid gap-y-16">{children}</section>
       </main>
     </>
