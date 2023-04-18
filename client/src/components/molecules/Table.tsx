7c762891161d020f7f8d879c67c1d9e07d1fd3c8
zoel-a <zulmyazhary32@gmail.com>
2023-04-18 16:50:52 +0800

    Add action additional props, handle an empty data

--- client/src/components/molecules/Table.tsx
+++ client/src/components/molecules/Table.tsx
@@ -6,18 +6,21 @@ import { useReactTable, getCoreRowModel, type ColumnDef } from "@tanstack/react-
 interface Props<T> extends React.HTMLAttributes<HTMLDivElement> {
   data: T[];
   columns: Array<ColumnDef<T>>;
+  action?: ColumnDef<T>;
 }
 
 const Table = <T extends object>(props: Props<T>) => {
-  const { data, columns, className, ...rest } = props;
+  const { data, columns, action, className, ...rest } = props;
   const datas = useMemo(() => data, [data]);
-  const headerColumns = useMemo(() => columns, []);
+  const headerColumns = useMemo(() => (action ? [...columns, action] : columns), []);
 
   const table = useReactTable({
     data: datas,
     columns: headerColumns,
     getCoreRowModel: getCoreRowModel()
   });
+
+  const rows = table.getRowModel().rows;
 
   return (
     <div className={clsx("relative overflow-x-auto", className)} {...rest}>
@@ -28,9 +31,15 @@ const Table = <T extends object>(props: Props<T>) => {
           ))}
         </thead>
         <tbody>
-          {table.getRowModel().rows.map(row => (
-            <TableRow key={row.id} row={row} />
-          ))}
+          {rows.length ? (
+            rows.map(row => <TableRow key={row.id} row={row} />)
+          ) : (
+            <tr className="whitespace-nowrap border-b dark:border-slate-700">
+              <td align="center" colSpan={100} className="px-6 py-5">
+                <span>No data available.</span>
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
