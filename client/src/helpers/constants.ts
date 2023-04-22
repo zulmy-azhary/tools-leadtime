a939a64f2997d185ce0034b90063c9f000495adc
zoel-a <zulmyazhary32@gmail.com>
2023-04-22 15:49:56 +0800

    Add qualityCheck constants

--- client/src/helpers/constants.ts
+++ client/src/helpers/constants.ts
@@ -1,4 +1,4 @@
-import type { TDamage, TProcess, TServiceAdvisor, TVendor } from "../types/common";
+import type { TDamage, TMainProcess, TProcess, TServiceAdvisor, TVendor } from "../types/common";
 
 export const SERVER_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
@@ -6,7 +6,7 @@ export const DAMAGE_TYPE: TDamage[] = ["Ringan", "Sedang", "Berat"];
 
 export const VENDOR: TVendor[] = ["WIS", "SPA"];
 
-export const PROCESS: TProcess[] = [
+export const MAIN_PROCESS: TMainProcess[] = [
   "Ketokan",
   "Removal",
   "Putty",
@@ -17,7 +17,7 @@ export const PROCESS: TProcess[] = [
   "Polishing"
 ];
 
-export const ALL_PROCESS: TProcess[] = ["Tunggu Teknisi", "Tunggu Part", ...PROCESS];
+export const ALL_PROCESS: TProcess[] = ["Tunggu Teknisi", "Tunggu Part", ...MAIN_PROCESS];
 
 export const SERVICE_ADVISOR: TServiceAdvisor[] = [
   "Ahmad Supardi",
@@ -28,4 +28,40 @@ export const SERVICE_ADVISOR: TServiceAdvisor[] = [
   "Musrin Noor",
   "Reza",
   "Syamsuryanan Amir"
+];
+
+// QC
+export const firstQualityCheck = [
+  "Hasil ketok",
+  "Anti karat",
+  "Hasil fenereging",
+  "Hasil pengelasan",
+  "Dimensi ruang mesin",
+  "Hasil pembongkaran"
+];
+
+export const secondQualityCheck = ["Hasil pelandaian", "Putty mark", "Pori-pori", "Hasil epoxy", "Stretch amplas"];
+
+export const thirdQualityCheck = [
+  "Hasil masking",
+  "Glowsy",
+  "Tidak terdapat pinhole",
+  "Tidak terdapat putty mark",
+  "Tidak terdapat creking",
+  "Tidak terdapat runs",
+  "Tidak terdapat fish eye",
+  "Tidak terdapat overspray",
+  "Tidak terdapat beda warna (Belang)",
+  "Tidak terdapat mottling",
+  "Tidak terdapat orange peal"
+];
+
+export const fourthQualityCheck = [
+  "Hasil ketokan",
+  "Hasil preparation",
+  "Hasil spraying",
+  "Hasil perakitan",
+  "Ketinggian cahaya lampu",
+  "Celah antara panel",
+  "Hasil pencucian"
 ];
