b073de8a5f72b614354bb678db877f3a64aa9643
zoel-a <zulmyazhary32@gmail.com>
2023-04-23 12:08:31 +0800

    Add endpoint

--- client/src/api/flowProcess.ts
+++ client/src/api/flowProcess.ts
@@ -1,8 +1,23 @@
-import type { TFlowProcess } from "../types";
+import type { TFlowProcessDataUnit, TFlowProcessData, TProcessItem } from "../types";
 import { axiosProtected } from "./axios";
 
-export const getAllFlowProcess = async (): Promise<TFlowProcess[]> => {
+export const getAllFlowProcess = async (): Promise<TFlowProcessDataUnit[]> => {
   return await axiosProtected()
-    .get("flowprocess/all")
+    .get("/flowprocess/all")
     .then(res => res.data.data);
+};
+
+export const getFlowProcessById = async (_id: string): Promise<TFlowProcessData> => {
+  return await axiosProtected()
+    .get(`/flowprocess/${_id}`)
+    .then(res => res.data.data);
+};
+
+export const createFlowProcess = async (data: TFlowProcessData) => {
+  return await axiosProtected().post("/flowprocess/create", data);
+};
+
+export const updateFlowProcess = async (data: TProcessItem & { workOrder: string }) => {
+  const { _id, ...rest } = data;
+  return await axiosProtected().put(`/flowprocess/${_id}`, rest);
 };
