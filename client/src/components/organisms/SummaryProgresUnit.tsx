231e84d9a42fee5671351629efb4a289b476c5ea
zoel-a <zulmyazhary32@gmail.com>
2023-05-01 14:58:30 +0800

    Update type, add table action, and refactor component

--- client/src/components/organisms/SummaryProgresUnit.tsx
+++ client/src/components/organisms/SummaryProgresUnit.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { ContentWrapper, Headers, Table } from "../molecules";
 import { Button, Card, Heading, Input } from "../atoms";
 import { IoSearch } from "react-icons/io5";
-import type { TSummary } from "../../types";
+import type { TSummaryData } from "../../types";
 import { summaryColumns } from "../../helpers/tableColumns";
 import clsx from "clsx";
+import type { ColumnDef } from "@tanstack/react-table";
+import { IoMdInformationCircle } from "react-icons/io";
 
 const SummaryProgresUnit: React.FC = () => {
   return (
@@ -26,33 +28,47 @@ const SummaryProgresUnit: React.FC = () => {
             Export CSV
           </Button>
         </div>
-        <Table data={summaryData} columns={summaryColumns} />
+        <Table data={summaryData} columns={summaryColumns} action={action} />
       </Card>
     </ContentWrapper>
   );
 };
 
-const summaryData: TSummary[] = [
+const action: ColumnDef<TSummaryData> = {
+  header: "Action",
+  cell: ({ row }) => (
+    <IoMdInformationCircle
+      onClick={() => alert(JSON.stringify(row.original, null, 2))}
+      className="cursor-pointer text-blue-500"
+      size="1.7em"
+    />
+  )
+};
+
+const summaryData: TSummaryData[] = [
   {
     workOrder: "20204/SWO/23/04/00001",
     serviceAdvisor: "Ahmad Supardi",
     vendor: "WIS",
     entryDate: "04 April 2023",
-    endDate: "06 April 2023"
+    endDate: "06 April 2023",
+    duration: "18 Jam"
   },
   {
     workOrder: "20204/SWO/23/04/00002",
     serviceAdvisor: "Reza",
     vendor: "SPA",
     entryDate: "01 April 2023",
-    endDate: "03 April 2023"
+    endDate: "03 April 2023",
+    duration: "2 Hari 6 Jam"
   },
   {
     workOrder: "20204/SWO/23/04/00003",
     serviceAdvisor: "Syamsuryanan Amir",
     vendor: "WIS",
     entryDate: "03 April 2023",
-    endDate: "04 April 2023"
+    endDate: "04 April 2023",
+    duration: "1 Hari 4 Jam"
   }
 ];
 
