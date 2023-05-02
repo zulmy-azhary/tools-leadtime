59fce59afba6265a4c34b0119df09f3b6b0c658d
zoel-a <zulmyazhary32@gmail.com>
2023-05-02 18:30:21 +0800

    Modify bad component's naming convention

--- client/src/components/molecules/index.ts
+++ client/src/components/molecules/index.ts
@@ -4,11 +4,11 @@ export { default as ImageWrapper } from "./ImageWrapper";
 export { default as LinkToPage } from "./LinkToPage";
 export { default as InputForm } from "./InputForm";
 export { default as SelectForm } from "./SelectForm";
-export { default as Headers } from "./Headers";
+export { default as Header } from "./Header";
 export { default as Table } from "./Table";
 export { default as Modal } from "./Modal";
 
 export { default as SidebarList } from "./SidebarList";
 export { default as UnitCard } from "./UnitCard";
-export { default as ProcesUnit } from "./ChartProcesUnit";
+export { default as ChartProcessUnit } from "./ChartProcessUnit";
 export { default as QuantityUnit } from "./ChartQuantityUnit";
