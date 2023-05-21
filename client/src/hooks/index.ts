7061faebb69ee4471d0293598d41d43166fef765
Muhammad Ridwan <ridhoang93@gmail.com>
2023-05-21 19:07:50 +0700

    adding useInstanceTable hook for modular export/import

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,4 +1,5 @@
 export { default as useToggle } from "./useToggle";
 export { default as useMediaQuery } from "./useMediaQuery";
 export { default as useOnClickOutside } from "./useOnClickOutside";
+export { default as useInstanceTable } from "./useInstanceTable";
 export { useDocumentTitle } from "./useDocumentTitle";
