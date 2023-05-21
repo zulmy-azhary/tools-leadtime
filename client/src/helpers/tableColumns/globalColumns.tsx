4efd6a424f3223041b442598e19095f879a1ecb8
Muhammad Ridwan <ridhoang93@gmail.com>
2023-05-22 00:53:39 +0700

    Modify global filter and sorting access for multiple columns.

--- client/src/helpers/tableColumns/globalColumns.tsx
+++ client/src/helpers/tableColumns/globalColumns.tsx
@@ -1,44 +1,62 @@
 export const workOrderColumn = {
   header: "Work Order",
-  accessorKey: "workOrder"
+  accessorKey: "workOrder",
+  enableGlobalFilter: true,
+  enableSorting: true
 };
 
 export const plateNumberColumn = {
   header: "Nomor Polisi",
-  accessorKey: "plateNumber"
+  accessorKey: "plateNumber",
+  enableGlobalFilter: false,
+  enableSorting: false
 };
 
 export const serviceAdvisorColumn = {
   header: "Service Advisor",
-  accessorKey: "serviceAdvisor"
+  accessorKey: "serviceAdvisor",
+  enableGlobalFilter: false,
+  enableSorting: false
 };
 
 export const vendorColumn = {
   header: "Team Vendor",
-  accessorKey: "vendor"
+  accessorKey: "vendor",
+  enableGlobalFilter: false,
+  enableSorting: false
 };
 
 export const entryDateColumn = {
   header: "Tanggal Masuk",
-  accessorKey: "entryDate"
+  accessorKey: "entryDate",
+  enableGlobalFilter: false,
+  enableSorting: true
 };
 
 export const processColumn = {
   header: "Proses",
-  accessorKey: "currentProcess"
+  accessorKey: "currentProcess",
+  enableGlobalFilter: false,
+  enableSorting: false
 };
 
 export const carTypeColumn = {
   header: "Tipe Kendaraan",
-  accessorKey: "carType"
+  accessorKey: "carType",
+  enableGlobalFilter: false,
+  enableSorting: false
 };
 
 export const damageTypeColumn = {
   header: "Jenis Kerusakan",
-  accessorKey: "damageType"
+  accessorKey: "damageType",
+  enableGlobalFilter: false,
+  enableSorting: false
 };
 
 export const handOverColumn = {
   header: "Janji Penyerahan",
-  accessorKey: "handOver"
+  accessorKey: "handOver",
+  enableGlobalFilter: false,
+  enableSorting: true
 };
